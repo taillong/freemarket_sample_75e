12cd5e52fdac0ad127a4a7d5365d688b04a513a1
taillong <taillong610@gmail.com>
2020-05-24 19:30:26 +0900

    image投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,20 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :condition, :delivery_fee, :prefecture_id, :date, :price, images_attributes: [:src])
   end
 end
