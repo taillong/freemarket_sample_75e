02ee8f9fabfad3fc3c68d386cf1a981e0586de93
taillong <taillong610@gmail.com>
2020-05-26 22:59:32 +0900

    カテゴリーのajax

--- app/assets/javascripts/channels/category.js
+++ app/assets/javascripts/channels/category.js
@@ -54,23 +54,33 @@
 $(function(){
   // selectタグに追加する予定のoptionタグを作成
   function appendOption(category){
-    let html = `<option value="${category.name}" data-category="${category.id}">
+    let html = `<option value="${category.name}" data-category_id="${category.id}">
                   ${category.name}
                 </option>`;
     return html
   }
-  // selectタグを追加
-  function appendSelect(insertOption){
-    let html = `<select class="sell-form__input--select" id="children_category" name="item[category]">
+  // 子カテゴリーのselectタグを追加
+  function appendSelectChild(insertOption){
+    let html = `<select class="sell-form__input--select" id="child_category" name="item[category]">
+                  <option value="">選択してください</option>
+                  ${insertOption}
+                </select>`;
+    $('.sell-form__wrapper').append(html);
+  }
+  
+  // 孫カテゴリーのselectタグを追加
+  function appendSelectGrandchild(insertOption){
+    let html = `<select class="sell-form__input--select" id="grandchild_category" name="item[category]">
                   <option value="">選択してください</option>
                   ${insertOption}
                 </select>`;
     $('.sell-form__wrapper').append(html);
   }
 
-  //カテゴリーのchangeイベントでajaxが発火する 
+  //親カテゴリーのchangeイベントでajaxが発火する 
   $('#parent_category').on('change', function(){
     let parentCategory = $('#parent_category').val();
+    // console.log(parentCategory)
     if (parentCategory != ""){
       $.ajax({
         url: 'get_category_children',
@@ -79,18 +89,47 @@ $(function(){
         dataType: 'json'
       })
       .done(function(children){
-        $('#children_category').remove();
+        $('#child_category').remove();
+        $('#grandchild_category').remove();
         let insertOption = ``
         children.forEach(function(child){
           insertOption += appendOption(child);
         })
-        appendSelect(insertOption);
+        appendSelectChild(insertOption);
       })
       .fail(function(){
         alert('カテゴリーの取得に失敗しました');
       })
     } else {
-      $('#children_category').remove();
+      $('#child_category').remove();
+      $('#grandchild_category').remove();
+    }
+  });
+
+  //子カテゴリーのchangeイベントでajaxが発火する 
+  $('.sell-form__wrapper').on('change', '#child_category',function(){
+    let childId = $('#child_category option:selected').data('category_id');
+    // console.log(childId)
+    if (childId != null ){
+      $.ajax({
+        url: 'get_category_grandchildren',
+        type: 'GET',
+        data: {child_id: childId},
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        $('#grandchild_category').remove();
+        let insertOption = ``
+        grandchildren.forEach(function(grandchild){
+          insertOption += appendOption(grandchild);
+        })
+        appendSelectGrandchild(insertOption);
+      })
+      .fail(function(){
+        alert('カテゴリーの取得に失敗しました');
+      })
+    } else {
+      $('#grandchild_category').remove();
     }
   });
 });
