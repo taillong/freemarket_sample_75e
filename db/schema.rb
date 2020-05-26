a7f9d6095318079bb840fd0d3f48817801c34285
taillong <taillong610@gmail.com>
2020-05-26 23:12:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_105052) do
+ActiveRecord::Schema.define(version: 2020_05_26_140438) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
