73876df07d4f89c3a8258f005208ed49cf53d3ca
sirokumasan <fsho2262@gmail.com>
2020-05-28 10:12:45 +0900

    userテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: {with:  /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/}
-  validates :password,  length: { minimum: 7 }, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,}\Z/i}
+  validates :password,  length: { minimum: 7 }, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,}\Z/}
   has_one   :person_info, dependent: :destroy, inverse_of: :user
   has_one   :address,     dependent: :destroy
 end
