1629230b6b1beda677c0160b17c7b976cc2b715f
taillong <taillong610@gmail.com>
2020-05-29 22:12:14 +0900

    画像が無いと登録できないようにしたよ

--- app/assets/javascripts/sell-form.js
+++ app/assets/javascripts/sell-form.js
@@ -1,34 +1,3 @@
-// $(function(){
-//   // 画像用のinputを生成する関数
-//   const buildFileField = (index)=> {
-//     const html = `<div data-index="${index}" class="js-file_group">
-//                     <input class="js-file" type="file"
-//                     name="item[images_attributes][${index}][src]"
-//                     id="item_images_attributes_${index}_src"><br>
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//     return html;
-
-//   }
-
-//   // file_fieldのnameに動的なindexをつける為の配列
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//   $('.sell-form__image__input').on('change', '.js-file', function(e) {
-//     // fileIndexの先頭の数字を使ってinputを作る
-//     $('.sell-form__image__input').append(buildFileField(fileIndex[0]));
-//     fileIndex.shift();
-//     // 末尾の数に1足した数を追加する
-//     // fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-//   });
-
-//   $('.sell-form__image__input').on('click', '.js-remove', function() {
-//     $(this).parent().remove();
-//     // 画像入力欄が0個にならないようにしておく
-//     if ($('.js-file').length == 0) $('.sell-form__image__input').append(buildFileField(fileIndex[0]));
-//   });
-// });
-
 $(function(){
   // file_fieldをつける
   function appendFileField(index){
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,8 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, :explanation, :price, :prefecture_id, :condition_id, :delivery_fee_id, :duration_id, :category_id, presence: true
+  validates_associated :images
+  validates :images, presence: true
 
   def self.brand_id_search(input)
     brand = Brand.find_by(name: input[:brand])
