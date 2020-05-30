803c0e48b19d01b1d2e40934d6c130f53df0ebab
sirokumasan <fsho2262@gmail.com>
2020-05-30 14:45:04 +0900

    user_showルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     post 'addresses',    to: 'users/registrations#create_address'
   end
   root 'items#index'
+  resources :users, only: :show
   resources :items, except: :index
 end
