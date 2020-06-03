205bd2d157192325fb8076dc0559fa8c1211064d
nnnakihisa <n.akihisa@gmail.com>
2020-06-03 10:11:14 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2020_05_26_105052) do
+ActiveRecord::Schema.define(version: 2020_05_27_164652) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
