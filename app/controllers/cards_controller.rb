186864270666902be263c75e56aa1a3f20fe784e
sirokumasan <fsho2262@gmail.com>
2020-06-04 16:07:08 +0900

    card_controller pay

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,24 @@
 class CardsController < ApplicationController
+  require 'payjp'
+
   def new
   end
+
+  def pay
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjpToken'].blank?
+      render :new
+    else 
+      customer = Payjp::Customer.create(
+        card: params['payjp-token']
+      )
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to root_path
+      else
+        render: new
+      end
+    end
+  end
+
 end
