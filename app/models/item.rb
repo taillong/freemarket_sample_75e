b443d3876846315505f177dc5e24b7e8aa3e8c24
taillong <taillong610@gmail.com>
2020-06-08 21:14:35 +0900

    update

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :brand, optional: true
 
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
-  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id' ,optional: true
   
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
