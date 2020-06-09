75a2e41d902f1a88b8660e9438057b26821273ac
sirokumasan <fsho2262@gmail.com>
2020-06-09 13:51:25 +0900

    cardモデルテスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    user_id { 1 }
-    customer_id { "MyString" }
-    card_id { "MyString" }
+    user
+    customer_id { "cus_52184be0aedc05c5a786694a8f21" }
+    card_id { "car_36c858caa2bc00c68a36648461d0" }
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it "すべてのカラムがあれば登録ができる" do
+      card = build(:card)
+      expect(card).to be_valid  
+    end
+
+    it "customer_idがないと登録できない" do
+      card = build(:card, customer_id: nil)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "card_idがないと登録できない" do
+      card = build(:card, card_id: nil)
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+
+    it "user_idがないと登録できない" do
+      card = build(:card, user_id: nil)
+      card.valid?
+      expect(card.errors[:user]).to include("を入力してください")
+    end
+  end
 end
