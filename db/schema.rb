d0a66ee759ed5167faddd349bd3f2468cf4e2a27
taillong <taillong610@gmail.com>
2020-06-10 10:54:54 +0900

    商品編集ページに飛べるようにリンクを付けた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_105052) do
+ActiveRecord::Schema.define(version: 2020_06_04_031234) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
